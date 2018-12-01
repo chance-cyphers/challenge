5bc240187eb2454ece54265bf828511b4dd0baee
chance <chance.a.cyphers@gmail.com>
2018-12-01 11:49:05 -0500

    Add camera page

--- lib/camera_page.dart
+++ lib/camera_page.dart
@@ -1,22 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
 
 class CameraPage extends StatefulWidget {
-
   @override
   State<StatefulWidget> createState() => _CameraPageState();
-
 }
 
 class _CameraPageState extends State<CameraPage> {
+  CameraController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    availableCameras().then((cameras) {
+      controller = CameraController(cameras[0], ResolutionPreset.medium);
+      controller.initialize().then((_) {
+        if (!mounted) {
+          print("not mounted");
+          return;
+        }
+        setState(() {});
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: const Text("the new page"),
-      ),
-      body: const Text("placeholder body"),
-    );
-  }
+    if (controller == null || !controller.value.isInitialized) {
+      return Container();
+    }
 
+    return AspectRatio(
+        aspectRatio: controller.value.aspectRatio,
+        child: CameraPreview(controller));
+  }
 }
